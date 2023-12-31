c2656836e015f12f56b31122b4bb5519dfea10c8
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2023-12-31 22:42:28 +0900

    update
    ヘッダーナビゲーションの閉じるボタンがうまく真ん中に重なってくれないけど、フィルタリングをWorksページに書いた

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const responsive_menu_btn = document.querySelector('.responsive_btn');
-responsive_menu_btn.addEventListener('click', Toggle_menu);
-
-function Toggle_menu() {
-    const header_menu_detail = document.querySelector('.header_nav');
+responsive_menu_btn.addEventListener('click', () => {
+  responsive_menu_btn.classList.toggle('active');
+  const header_menu_detail = document.querySelector('.header_nav');
     header_menu_detail.classList.toggle('menu_active');
-  }
+});
+
+//function Toggle_menu() {
+//    const header_menu_detail = document.querySelector('.header_nav');
+//    header_menu_detail.classList.toggle('menu_active');
+
+//  }

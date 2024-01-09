23f5b86e38e898f726fa758861e0e887df07e914
Suyuzui <83820092+Suyuzui@users.noreply.github.com>
2024-01-09 23:06:17 +0900

    update
    Version１は完成しました。

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//ヘッダーナビゲーションの設定
 const responsive_menu_btn = document.querySelector('.responsive_btn');
 responsive_menu_btn.addEventListener('click', () => {
   responsive_menu_btn.classList.toggle('active');
@@ -10,3 +11,80 @@ responsive_menu_btn.addEventListener('click', () => {
 //    header_menu_detail.classList.toggle('menu_active');
 
 //  }
+
+
+//フィルタリング
+document.addEventListener('DOMContentLoaded', () => {
+  // ボタンを選択
+  document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('click', () => {
+      // value属性の値を取得
+      const target = button.getAttribute('value');
+
+      // 全てのli要素を調べる
+      document.querySelectorAll('#lists li').forEach(li => {
+        // 一度全て非表示にする
+        li.style.opacity = 0;
+        li.style.display = 'none';
+
+        // フィルタリングの条件を満たしているか
+        if (li.classList.contains(target) || target === 'all') {
+          // 条件を満たしている場合は再表示
+          li.style.display = 'block';
+          li.style.opacity = 1;
+        }
+      });
+    });
+  });
+});
+
+
+
+// JavaScript code using ES6 syntax
+// ES6の記法を使用してボタンがクリックされたときに色を変更する関数を定義
+const changeButtonColor = () => {
+  const buttons = document.querySelectorAll('.button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+    });
+  });
+};
+
+// ページが読み込まれたら色を変更する関数を実行
+document.addEventListener('DOMContentLoaded', () => {
+  changeButtonColor();
+});
+
+
+//キャプション
+document.addEventListener('DOMContentLoaded', () => {
+  const duration = 300;
+  const images = document.querySelectorAll('.images p');
+
+  images.forEach((image, index) => {
+    image.addEventListener('mouseover', () => {
+      animateElements(image, 1);
+    });
+
+    image.addEventListener('mouseout', () => {
+      animateElements(image, 0);
+    });
+
+    image.addEventListener('click', () => {
+      const link = image.closest('a').getAttribute('href');
+      window.location.href = link;
+    });
+  });
+
+  function animateElements(image, opacity) {
+    const elementsToAnimate = image.querySelectorAll('span');
+    elementsToAnimate.forEach((element) => {
+      element.style.transition = `opacity ${duration}ms`;
+      element.style.opacity = opacity;
+    });
+  }
+});
+
